805aaa867cb0474976bda07c14e2c88384db616f
Peeter <peeter.pihus@khk.ee>
2018-05-28 18:17:19 +0200

    initial commit

---
+++ expect.js
@@ -0,0 +1,10 @@
+expect(add(0.1, 0.2)).toBe(0.3);
+
+expect(add(0.1, 0.2)).not.toBe(0.1);
+
+add(0.1, 0.2) === 0.3;
+
+it('should consider this spec'. function(){
+    expect(true).toBe(true);
+    expect(false).not.toBe(false);
+});
